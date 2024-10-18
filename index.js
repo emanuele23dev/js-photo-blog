d5c8b0df6f7101d78834832bc555b1cd51e9fbaf
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-18 15:17:20 +0200

    end photo blog

--- index.js
+++ index.js
@@ -6,13 +6,43 @@
 
 console.log('ciao');
 
-const photosEl = document.querySelector('.photos')
+const rowEl = document.querySelector(".row");
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-.then(response => {
 
-    const photos = response.data 
+  .then((response) => {
+    const photos = response.data;
     console.log(photos);
-    
 
-}).catch(err => console.error(err))
+    let photoElements = "";
+
+    photos.forEach((photo) => {
+      const { url, title } = photo;
+      console.log(url, title);
+
+      const markup = `
+    <div class="col-4 photos">
+        
+            <div class="polaroid">
+
+                <img width="25px" class="pin" src="./img/pin.svg" alt="">
+               
+                <div class="quadrato">
+                    <img src="${url}" alt="">
+                </div>
+                    
+               <div class="text text-center mt-3">
+                <p>${title}</p>
+               </div>
+
+            </div>
+    </div>
+    `
+      photoElements += markup;
+    });
+
+    rowEl.innerHTML = photoElements;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
